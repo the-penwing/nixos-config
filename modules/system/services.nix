42965de25a59c06e63bf7f1786c177aa843ba8e1
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-04-15 20:52:41 +1000

    Fixed Start Hyprland

--- modules/system/services.nix
+++ modules/system/services.nix
@@ -19,7 +19,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd '${pkgs.hyprland}/bin/Hyprland'";
         user = "greeter";
       };
     };
