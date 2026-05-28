baadd8858394d11ad746698e0e868dbee0bcab23
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-05-28 19:40:31 +1000

    changed timeformat

--- dotfiles/shell/zsh/zshrc.d/10-environment.zsh
+++ dotfiles/shell/zsh/zshrc.d/10-environment.zsh
@@ -32,7 +32,7 @@ DISABLE_FZF_AUTO_COMPLETION="true"
 DISABLE_FZF_KEY_BINDINGS="false"
 FZF_DEFAULT_OPTS="--color=dark --color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f --color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7 --style full --preview 'bat --style=numbers --color=always --line-range :500 {}'"
 SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent"
-TIMEFMT="\\nreal %E\\nuser %U\\nsys %S\\n"
+TIMEFMT=$'\nreal %*E\nuser %*U\nsys %*S\n'
 BAT_THEME="Dracula"
 
 set +a
