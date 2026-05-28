0fa053376bfbde6c39d89029e51428697f95a3a7
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-05-28 19:21:53 +1000

    moved HDMI 1 above builtin display

--- dotfiles/desktop/hyprland/supercoolconfig/monitors.lua
+++ dotfiles/desktop/hyprland/supercoolconfig/monitors.lua
@@ -6,24 +6,14 @@ hl.monitor({
 })
 
 hl.monitor({
-	output = "HDMI-A-!",
+	output = "HDMI-A-1",
 	mode = "1920x1080@60",
 	position = "0x-1080",
 	scale = "1",
 })
--- Retroid Pocket 4 Pro (1334x750)
--- hl.monitor({ output = "virtual", mode = "1334x750@60", position = "-1334x0", scale = "1" })
 
--- Family iPad (2048x1536)
+-- iPad Mini (2048x1536)
 -- hl.monitor({ output = "virtual", mode = "2048x1536@60", position = "-2048x0", scale = "1" })
-
--- for workspace = 1, 10 do
--- 	local monitor = workspace == 10 and "HDMI-A-" or "virtual" or "eDP-1"
--- 	hl.workspace_rule({
--- 		workspace = tostring(workspace),
--- 		monitor = monitor,
--- 	})
--- end
 
 for workspace = 1, 10 do
 	hl.workspace_rule({
