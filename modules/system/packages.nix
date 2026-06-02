95bc42252673a16d23a97dfff76eb8a05f0680ea
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-06-02 16:20:30 +1000

    update package list location cleanup part 5

--- modules/system/packages.nix
+++ modules/system/packages.nix
@@ -1,11 +1,11 @@
 # System package wiring.
 #
 # Purpose:
-# - Pull all package categories from ../../nix/packages.nix
+# - Pull all package categories from .package-list.nix
 # - Keep the package catalogue as a single source of truth
 { pkgs, lib, ... }:
 let
-  packageSet = import ../../nix/packages.nix { inherit pkgs lib; };
+  packageSet = import ./package-list.nix { inherit pkgs lib; };
 in
 {
   environment.systemPackages = packageSet.systemPackages;
