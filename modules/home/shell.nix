4fcb2fc3f93a795236dc1c18555b4757b69faad2
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-06-03 15:42:41 +1000

    home changes

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -9,20 +9,8 @@
 {
   home.packages = with pkgs; [
     lazygit
-    sassc
-    zsh-autosuggestions
-    zsh-syntax-highlighting
+    pass-git-helper
   ];
-
-  home.sessionPath = [
-    "$HOME/.npm-global/bin"
-    "$HOME/.cargo/bin"
-    "$HOME/.local/bin"
-  ];
-
-  programs.direnv = {
-    enable = true;
-  };
 
   programs.starship = {
     enable = true;
@@ -40,6 +28,20 @@
       identityFile = "~/.ssh/id_ed25519";
     };
   };
-
-  programs.git.enable = true;
+  programs.git = {
+    enable = true;
+    extraConfig = {
+      credential.helper = "!pass-git-helper $@";
+    };
+  };
+  # Define how Git hosts map to entries inside your password-store (~/.password-store/)
+  xdg.configFile."pass-git-helper/git-pass-mapping.ini".text = ''
+    [github.com]
+    target=dev/github
+  '';
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+    pinentryPackage = pkgs.pinentry-curses; # Forces a TTY prompt that works safely in the background
+  };
 }
