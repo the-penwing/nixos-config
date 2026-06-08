cd9cf9c3b962eb2dda963b258f9ccb57cf2c320d
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-06-08 22:26:29 +1000

    make git homemanaged

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -30,7 +30,19 @@
   };
   programs.git = {
     enable = true;
+    signing = {
+      key = "3949612C4B58A93F3DCD7488A11420689178B907";
+      signByDefault = true;
+    };
     settings = {
+      init.defaultBranch = "main";
+      user = {
+        name = "Ben van Leeuwen";
+        email = "benvanleeuwen01@gmail.com";
+      };
+      gpg = {
+        format = "openpgp";
+      };
       credential.helper = "!pass-git-helper $@";
     };
   };
