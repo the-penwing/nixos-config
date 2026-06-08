e47a5012653b178672c036af046ee689b3e2342b
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-06-08 22:11:39 +1000

    removed duped mason installs

--- dotfiles/editor/nvim/lua/plugins/mason.lua
+++ dotfiles/editor/nvim/lua/plugins/mason.lua
@@ -7,30 +7,6 @@ return {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     opts = {
       ensure_installed = {
-        -- Language Servers (these handle their own diagnostics)
-        "lua-language-server",
-        "rust-analyzer",
-        "python-lsp-server",
-        "bash-language-server",
-        "marksman",
-        "clangd",
-        "rnix-lsp",
-        "html-lsp",
-        "typescript-language-server",
-        "yaml-language-server",
-
-        -- Formatters only
-        "stylua",
-        "black",
-        "shfmt",
-        "prettier",
-        "nixfmt",
-        "clang-format",
-
-        -- Debuggers
-        "debugpy",
-        "codelldb",
-
         -- Tree-sitter
         "tree-sitter-cli",
       },
