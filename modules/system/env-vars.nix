52c98fd11e9baa47d42469ec0bce35c63d34dff0
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-06-13 17:20:27 +1000

    added GSETTINGS_SCHEMA_DIR var

--- modules/system/env-vars.nix
+++ modules/system/env-vars.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   environment.variables = {
@@ -9,6 +9,6 @@
     SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/ssh-agent";
     GPG_TTY = "$(tty)";
     DOCKER_HOST = "unix:///run/user/1000/podman/podman.sock";
-
+    GSETTINGS_SCHEMA_DIR = "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}/glib-2.0/schemas";
   };
 }
