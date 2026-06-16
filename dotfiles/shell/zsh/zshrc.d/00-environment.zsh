eff924355a4f48a7e8ad957c90839845bcfcf990
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-06-16 14:18:02 +1000

    updated fzf default

--- dotfiles/shell/zsh/zshrc.d/00-environment.zsh
+++ dotfiles/shell/zsh/zshrc.d/00-environment.zsh
@@ -5,7 +5,11 @@ export FZF_BASE="$HOME/.fzf"
 export FZF_DEFAULT_COMMAND="fd"
 export DISABLE_FZF_AUTO_COMPLETION="false"
 export DISABLE_FZF_KEY_BINDINGS="false"
-export FZF_DEFAULT_OPTS="--color=dark --color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f --color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7 --style full --preview 'bat --style=numbers --color=always --line-range :500 {}'"
+export FZF_DEFAULT_OPTS="--color=dark \
+  --color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f \
+  --color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7 \
+  --style default \
+  --preview 'bat --style=numbers --color=always --line-range :500 {}'"
 export TIMEFMT=$'\nreal %*E\nuser %*U\nsys %*S\n'
 export BAT_THEME="Dracula"
 export SDKROOT="/home/benvl/dev/resources/SDKs/MacOSX26.1.sdk"
