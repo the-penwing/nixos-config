45736b53beabedfae6f50cbd081a32cdd23aaafd
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-06-25 09:44:51 +1000

    changed fullscreen keybind

--- dotfiles/desktop/hyprland/supercoolconfig/keybinds.lua
+++ dotfiles/desktop/hyprland/supercoolconfig/keybinds.lua
@@ -25,12 +25,12 @@ local core_binds = {
 	{ mods = mainMod, key = "Q", dispatcher = hl.dsp.window.close() },
 	{ mods = mainMod .. " + SHIFT", key = "Q", dispatcher = hl.dsp.exit() },
 	{
-		mods = mainMod,
+		mods = mainMod .. "+ SHIFT",
 		key = "F",
 		dispatcher = hl.dsp.window.fullscreen({ mode = "fullscreen", action = "toggle" }),
 	},
 	{
-		mods = mainMod .. " + SHIFT",
+		mods = mainMod,
 		key = "F",
 		dispatcher = hl.dsp.window.fullscreen({ mode = "maximized", action = "toggle" }),
 	},
