8e48846eef2af223ce514827cea3ca7518bb0d24
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-06-25 11:11:44 +1000

    edit tmux config

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -4,6 +4,7 @@
     enable = true;
     shortcut = "a";
     baseIndex = 1;
+    keyMode = "vi";
     clock24 = true;
     mouse = true;
     historyLimit = 20000;
@@ -29,6 +30,22 @@
 
     extraConfig = ''
       set -g renumber-windows on
+      set -g status-left-length 50
+
+      bind-key "T" run-shell "sesh connect \"$(
+        sesh list --icons | fzf-tmux -p 80%,70% \
+          --no-sort --ansi --border-label ' sesh ' --prompt '⚡  ' \
+          --header '  ^a all ^t tmux ^g configs ^x zoxide ^d tmux kill ^f find' \
+          --bind 'tab:down,btab:up' \
+          --bind 'ctrl-a:change-prompt(⚡  )+reload(sesh list --icons)' \
+          --bind 'ctrl-t:change-prompt(🪟  )+reload(sesh list -t --icons)' \
+          --bind 'ctrl-g:change-prompt(⚙️  )+reload(sesh list -c --icons)' \
+          --bind 'ctrl-x:change-prompt(📁  )+reload(sesh list -z --icons)' \
+          --bind 'ctrl-f:change-prompt(🔎  )+reload(fd -H -d 2 -t d -E .Trash . ~)' \
+          --bind 'ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(⚡  )+reload(sesh list --icons)' \
+          --preview-window 'right:55%' \
+          --preview 'sesh preview {}'
+      )\""
     '';
   };
 }
