c5270cc3ce0c2bc6cab92e958a6ba9c59d60c21a
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-07-04 15:58:27 +1000

    added usbc monitor (i have another port hope its just one output or its gonna get messy)

--- dotfiles/desktop/hyprland/supercoolconfig/monitors.lua
+++ dotfiles/desktop/hyprland/supercoolconfig/monitors.lua
@@ -12,6 +12,13 @@ hl.monitor({
 	scale = "1",
 })
 
+hl.monitor({
+	output = "DP-2",
+	mode = "1920x1080@60",
+	position = "0x-1080",
+	scale = "1",
+})
+
 -- iPad Mini (2048x1536)
 -- hl.monitor({ output = "virtual", mode = "2048x1536@60", position = "-2048x0", scale = "1" })
 
@@ -23,7 +30,7 @@ for workspace = 1, 10 do
 end
 
 for workspace = 11, 20 do
-	local monitor = "HDMI-A-1" or "virtual" or "eDP-1"
+	local monitor = "HDMI-A-1" or "DP-2" or "virtual" or "eDP-1"
 	hl.workspace_rule({
 		workspace = tostring(workspace),
 		monitor = monitor,
