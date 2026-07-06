846e4c63d5dfed6e0204e25cb97c90ebb72e422a
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-07-06 22:14:43 +1000

    removed redundant block in rust nvim config

--- dotfiles/editor/nvim/lua/plugins/langs/rust.lua
+++ dotfiles/editor/nvim/lua/plugins/langs/rust.lua
@@ -1,12 +1,6 @@
 -- lua/plugins/langs/rust.lua
 return {
   {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = { "rust-analyzer" },
-    },
-  },
-  {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "rust" })
