37fd4bf17cd500f0a85a8fbc66e881be6b2a9cff
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-07-13 20:57:15 +1000

    autostart hyprpaper

--- dotfiles/desktop/hyprland/supercoolconfig/autostart-enviroment.lua
+++ dotfiles/desktop/hyprland/supercoolconfig/autostart-enviroment.lua
@@ -4,6 +4,7 @@ local exec_once_commands = {
 	"dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP",
 	"solaar --window=hide -w hide",
 	"kando",
+	"hyprpaper",
 	"waybar",
 }
 
