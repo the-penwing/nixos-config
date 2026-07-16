a4e52244b2d42d384762d3e4a66d97eb0493023a
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-07-16 13:04:45 +1000

    get brightness controls working

--- modules/system/hardware.nix
+++ modules/system/hardware.nix
@@ -2,7 +2,7 @@
 #
 # Purpose:
 # - Keep hardware enablement separate from service and package concerns
-{...}: {
+{pkgs, ...}: {
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -23,7 +23,14 @@
 
   hardware.bluetooth.enable = true;
 
+  hardware.acpilight = {
+    enable = true;
+  };
+
   services.udev.extraRules = ''
     SUBSYSTEMS=="usb", ATTRS{idVendor}=="2e8a", MODE="0666", TAG+="uaccess"
+
+    # Securely point directly to the Nix store binaries so the sandbox validator passes
+    ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="amdgpu_bl1", RUN+="${pkgs.coreutils}/bin/chmod g+w /sys/class/backlight/%k/brightness", RUN+="${pkgs.coreutils}/bin/chgrp video /sys/class/backlight/%k/brightness"
   '';
 }
