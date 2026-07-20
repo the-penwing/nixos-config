5843f78d449e37dabec5f95c0ac1b3b4e2709e8f
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-07-20 18:17:57 +1000

    reorgnaise and update shell env vars

--- dotfiles/shell/zsh/config-dir/zshrc.d/00-environment.zsh
+++ dotfiles/shell/zsh/config-dir/zshrc.d/00-environment.zsh
@@ -1,26 +1,28 @@
 # ============================================================================
 # Environment Variables
 # ============================================================================
+# CORE
+export BAT_THEME="Dracula"
 export FZF_BASE="$HOME/.fzf"
 export FZF_DEFAULT_COMMAND="fd"
-export DISABLE_FZF_AUTO_COMPLETION="false"
-export DISABLE_FZF_KEY_BINDINGS="false"
 export FZF_DEFAULT_OPTS="--color=dark \
   --color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f \
   --color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7 \
   --style default \
   --preview 'bat --style=numbers --color=always --line-range :500 {}'"
+export DISABLE_FZF_AUTO_COMPLETION="false"
+export DISABLE_FZF_KEY_BINDINGS="false"
 export TIMEFMT=$'\nreal %*E\nuser %*U\nsys %*S\n'
-export BAT_THEME="Dracula"
+# SYSTEM SPECIFIC
+export PATH="$PATH:$HOME/nixos-config/scripts"
+# SDKs
 export SDKROOT="/home/benvl/dev/resources/SDKs/MacOSX26.1.sdk"
 export PICO_SDK_PATH="/home/benvl/dev/hardware/pico2/cc/pico-sdk"
-export GPG_TTY=$(tty)
-export PATH="$PATH:$HOME/.npm-global/bin:$HOME/.cargo/bin:$HOME/nixos-config/scripts"
-export XDG_DATA_DIRS="$HOME/.local/share:$XDG_DATA_DIRS"
+# WM / DE
 export BEMOJI_PICKER_CMD="fuzzel -d"
-
+export GPG_TTY=$(tty)
 # ============================================================================
-# History Management 
+# History Management
 # ============================================================================
 export HISTSIZE=50000
 export SAVEHIST=50000
