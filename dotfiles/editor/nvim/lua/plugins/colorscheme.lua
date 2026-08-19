e6b144efc35c4b9348bb841e48958978c19b0054
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-08-19 12:17:54 +1000

    manual transparency

--- dotfiles/editor/nvim/lua/plugins/colorscheme.lua
+++ dotfiles/editor/nvim/lua/plugins/colorscheme.lua
@@ -5,9 +5,9 @@ return {
 		priority = 1000,
 		config = function()
 			vim.cmd.colorscheme("dracula")
+
+			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		end,
-	},
-	{
-		"xiyaowong/transparent.nvim",
 	},
 }
