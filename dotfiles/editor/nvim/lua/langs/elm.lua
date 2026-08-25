47f90aae4a732a6098544404cf4c3f0a5c148606
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-08-25 16:07:40 +1000

    updated elm neovim

--- dotfiles/editor/nvim/lua/langs/elm.lua
+++ dotfiles/editor/nvim/lua/langs/elm.lua
@@ -4,6 +4,16 @@ return {
 		servers = {
 			elmls = {
 				cmd = { "elm-language-server" },
+				settings = {
+					elmLS = {
+						elmPath = "elm",
+						elmFormatPath = "elm-format",
+						elmTestPath = "elm-test",
+						elmReviewPath = "elm-review",
+						disableElmLSDiagnostics = false,
+						onlyUpdateDiagnosticsOnSave = false,
+					},
+				},
 			},
 		},
 	},
