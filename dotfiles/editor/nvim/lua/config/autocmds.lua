c07b8279faf31956d5090de21e419f5a6a7225bf
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-09-15 18:07:04 +1000

    replace fzf with a telescope intergration

--- dotfiles/editor/nvim/lua/config/autocmds.lua
+++ dotfiles/editor/nvim/lua/config/autocmds.lua
@@ -2,10 +2,8 @@
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(args)
 		local opts = { buffer = args.buf }
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
---
+++ dotfiles/editor/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,20 @@
+-- lua/plugins/telescope.lua
+return {
+	"nvim-telescope/telescope.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
+	config = function()
+		local telescope = require("telescope")
+		telescope.setup({})
+		telescope.load_extension("fzf")
+
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+		vim.keymap.set("n", "gr", builtin.lsp_references, {})
+		vim.keymap.set("n", "gd", builtin.lsp_definitions, {})
+	end,
+}
