69b92f37838f9afa921359c6eb6c56428b7bf60c
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-09-20 19:31:23 +1000

    atuin shell intergration

---
+++ dotfiles/shell/zsh/config-dir/zshrc.d/55-atuin.zsh
@@ -0,0 +1,7 @@
+# ============================================================================
+# Atuin: shell history sync
+# ============================================================================
+eval "$(atuin init zsh --disable-up-arrow)"
+
+# Ensure clean exit status for sourcing
+true
