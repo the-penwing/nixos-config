199de5b58ffca341cf17c8ef3a9110e4134f4c83
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-09-23 13:33:20 +1000

    finish removing kotlin from nvim

--- dotfiles/editor/nvim/lua/plugins/lint.lua
+++ dotfiles/editor/nvim/lua/plugins/lint.lua
@@ -11,9 +11,6 @@ return {
 			"--",
 		}
 
-		local detekt_linter = lint.linters.detekt
-		detekt_linter.args = { "--input" } -- append_fname=true will add the filename right after this
-
 		lint.linters_by_ft = {
 			markdown = { "markdownlint-cli2" },
 			lua = { "selene" },
